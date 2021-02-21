88303dec285a789a86124904c7202f889c3252d7
rayehe <hafezifar.r@gmail.com>
2021-02-21 00:57:50 -0700

    Question 3 and 4 part 1 added

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -72,7 +72,7 @@ for row in df_price.index:
     if df_price["z-score"][row] > 3 or df_price["z-score"][row] < -3:
         outlier_price["price"].append(df_price["price"][row])
         outlier_price["z-score"].append(df_price["z-score"][row])
-print(f"detecting outliers by z-score: {outlier_price}")
+# print(f"detecting outliers by z-score: {outlier_price}")
 
 # Detecting outliers using 1.5*IQR:
 Q1 = df_price["price"].quantile(0.25)
@@ -84,7 +84,63 @@ outlier_price2 = []
 for row in df_price.index:
     if df_price["price"][row] < Q1 - IQR2 or df_price["price"][row] > Q3 + IQR2:
         outlier_price2.append(df_price["price"][row])
-print(f"detecting outliers by IQR: {outlier_price2}")
+# print(f"detecting outliers by IQR: {outlier_price2}")
 
 # *********************************************************************************
 # Question3
+
+stat_length = {"min": [], "max": [], "mean": [], "range": []}
+stat_compressionRatio = {"min": [], "max": [], "mean": [], "range": []}
+
+# Calculating min,max, and mean of Length attribute:
+statLength = df["length"].describe()
+stat_length["min"].append(statLength[3])
+stat_length["max"].append(statLength[7])
+stat_length["mean"].append(statLength[1])
+stat_length["range"].append(statLength[7] - statLength[3])
+# print("statistic for length attribute:", stat_length)
+
+# Calculating min,max, and mean of compression-ratio attribute:
+statCompression = df["compression-ratio"].describe()
+stat_compressionRatio["min"].append(statCompression[3])
+stat_compressionRatio["max"].append(statCompression[7])
+stat_compressionRatio["mean"].append(statCompression[1])
+stat_compressionRatio["range"].append(statCompression[7] - statCompression[3])
+# print("statistic for compression-ratio attribute:", stat_compressionRatio)
+
+# Min-max normalization for length attribute to [0, 1]:
+min_max_Length = []
+for row in df.index:
+    min_max_Length.append((df["length"][row] - statLength[3]) / (statLength[7] - statLength[3]))
+df["Min-max Length"] = min_max_Length
+# print("Min-max Length:\n", df["Min-max Length"])
+
+# z-score normalization for length attribute:
+z_score_Length = []
+for row in df.index:
+    z_score_Length.append((df["length"][row] - statLength[1]) / statLength[2])
+df["z-score Length"] = z_score_Length
+# print("z-score Length:\n", df["z-score Length"])
+
+# Min-max normalization for compression-ratio attribute to [0, 1]:
+min_max_compression = []
+for row in df.index:
+    min_max_compression.append((df["compression-ratio"][row] - statCompression[3]) / (statCompression[7] - statCompression[3]))
+df["Min-max compression-ratio"] = min_max_compression
+# print("Min-max compression-ratio:\n", df["Min-max compression-ratio"])
+
+# z-score normalization for compression-ratio attribute:
+z_score_compression = []
+for row in df.index:
+    z_score_compression.append((df["compression-ratio"][row] - statCompression[1]) / statCompression[2])
+df["z-score compression-ratio"] = z_score_compression
+# print("z-score compression-ratio:\n", df["z-score compression-ratio"])
+
+# print(df["Min-max Length"].describe())
+# print(df["z-score Length"].describe())
+# print(df["Min-max compression-ratio"].describe())
+# print(df["z-score compression-ratio"].describe())
+# ***********************************************************************************************
+# Question 4
+
+print(df.corrwith(df['price'], axis='index', method="pearson"))
